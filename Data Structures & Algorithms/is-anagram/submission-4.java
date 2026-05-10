20a74a5ed9cf03fc754549d2f6ff95b647ddf64b
Tiger <98557920+hasanmertyilmaz@users.noreply.github.com>
2026-05-10 19:45:42 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int firstNumber = s.length();
+        int secondNumber = t.length();
+        if(firstNumber != secondNumber) {
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i = 0; i < firstNumber; i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < secondNumber; i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
